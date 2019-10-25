f67bac717dc6777b24bddd0f83bd41b4b4a04ffe
Yurii Momotenko <momotenko.yuriy@gmail.com>
2019-10-25 22:25:07 +0300

    Created project folder for task 1

---
+++ Task_1. University simulation/Task_1. University simulation.cpp
@@ -0,0 +1,15 @@
+//Variant 23
+//Task 1
+//Simulation of university
+//Momotenko Yurii
+//K-29
+
+#include <iostream>
+#include "Knowledge.h"
+
+int main()
+{
+	Knowledge a("Discrete Mathematics", 4);
+	std::cout<<a++;
+	++a;
+}
