90cad8068f27a5b327c7d04b0feacf3edb136598
Yurii Momotenko <momotenko.yuriy@gmail.com>
2019-10-26 22:19:34 +0300

    Added and implemented getters

--- Task_1. University simulation/Task.cpp
+++ Task_1. University simulation/Task.cpp
@@ -2,19 +2,72 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include "Student.h"
 
-Task::Task(const std::string& task_name, size_t maximum_points, size_t level_of_complexity, std::vector<Knowledge> minimal_required_knowledges) {
+Task::Task(const std::string& task_name, size_t maximum_points, std::vector<Knowledge> minimal_required_knowledges) {
 	name = task_name;
 	max_points = maximum_points;
-	complexity = level_of_complexity;
-	std::sort(minimal_required_knowledges.begin(), minimal_required_knowledges.end(),
-		[](Knowledge& a, Knowledge& b) {
-			return (a < b);
-		});
+	//std::sort(minimal_required_knowledges.begin(), minimal_required_knowledges.end(),
+	//	[](Knowledge& a, Knowledge& b) {
+	//		return (a < b);
+	//	});
 	required_knowledges = minimal_required_knowledges;
+	for (size_t i = 0, size = required_knowledges.size(); i < size; ++i) {
+		complexity += minimal_required_knowledges[i].get_level();
+	}
+	if (minimal_required_knowledges.size())
+		complexity = (size_t)((complexity / minimal_required_knowledges.size())*0.5 + maximum_points * 0.05);
+	else
+		complexity = (size_t)(maximum_points * 0.05);
 }
+
+Task::Task(const Task& to_copy) :
+	name(to_copy.name),
+	max_points(to_copy.max_points),
+	complexity(to_copy.complexity),
+	required_knowledges(to_copy.required_knowledges),
+	time(to_copy.time)
+{	}
 
 //TODO:complete overloading
 bool Task::operator<(const Task& to_compare) {
 	return (complexity < to_compare.complexity);
+}
+
+Task& Task::operator--() {
+	if (time)
+		--time;
+	return *this;
+}
+
+Task Task::operator--(int) {
+	Task ans = *this;
+	if (time)
+		--time;
+	return ans;
+}
+
+size_t Task::time_left() {
+	return time;
+}
+
+void Task::calculateTime(Student& to_calc){
+	double temp = 0;
+	std::vector<Knowledge> students_knowledges = to_calc.get_knowledge_list();
+	for (size_t i = 0, size = required_knowledges.size(); i < size; ++i) {
+		temp = temp + (required_knowledges[i].get_level() / students_knowledges[i].get_level());
+	}
+
+	temp /= required_knowledges.size();
+	temp *= 1.5;
+	temp += complexity * 1.5;
+	time = (size_t)temp;
+}
+
+std::vector<Knowledge> Task::get_knowledge_list() {
+	return required_knowledges;
+}
+
+size_t Task::get_points() {
+	return max_points;
 }
--- Task_1. University simulation/Task.h
+++ Task_1. University simulation/Task.h
@@ -10,6 +10,9 @@
 #include <vector>
 #include <string>
 #include "Knowledge.h"
+//#include "Student.h"
+
+class Student;
 
 /*!
 	\brief Class Task
@@ -19,12 +22,20 @@ class Task{
 private:
 	std::string name;
 	size_t max_points;
-	size_t complexity;
+	size_t complexity = 0;
 	std::vector<Knowledge> required_knowledges;
 	size_t time = 0;
 public:
-	Task(const std::string& task_name, size_t maximum_points, size_t level_of_complexity, std::vector<Knowledge> minimal_required_knowledges);
+	Task() = default;
+	Task(const std::string& task_name, size_t maximum_points, std::vector<Knowledge> minimal_required_knowledges);
+	Task(const Task& to_copy);
 	bool operator<(const Task& to_compare);
+	Task& operator--();
+	Task operator--(int);
+	size_t time_left();
+	size_t get_points();
+	std::vector<Knowledge> get_knowledge_list();
+	void calculateTime(Student& to_calc);
 };
 
 #endif // !TASK_H
