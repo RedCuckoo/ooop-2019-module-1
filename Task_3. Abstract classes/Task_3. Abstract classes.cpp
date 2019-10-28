89abef35d4652b9eab4d40f2f11064b909eb4501
Yurii Momotenko <momotenko.yuriy@gmail.com>
2019-10-28 13:20:30 +0200

    Created base for the task, understood the task

--- Task_3. Abstract classes/Task_3. Abstract classes.cpp
+++ Task_3. Abstract classes/Task_3. Abstract classes.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+double S;
+
+class Base {
+	static size_t maxId;
+	size_t N;
+public:
+	Base() {
+		N = maxId++;
+	}
+	~Base(){
+		S = 2 * S + N - 23;
+	}
+
+
+	virtual void func() = 0;
+	class Alpha {
+		class Red {
+
+		};
+		class Green {
+
+		};
+	public:
+
+	};
+	class Beta {
+
+	};
+};
+
+
+size_t Base::maxId = 1;
+
 int main()
 {
 
